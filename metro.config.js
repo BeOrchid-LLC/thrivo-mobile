7951574aa4e4080b161097cbf31964ba275199df
Edward-Precious Omegbu <eomegbu@gmail.com>
2026-07-16 14:07:51 +0100

    Configure Metro build directory exclusions

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,14 @@ const { withNativeWind } = require("nativewind/metro");
 const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
 
 const config = getDefaultConfig(__dirname);
+config.resolver.useWatchman = false;
+config.resolver.blockList = [
+  /android[\\/]\.gradle[\\/].*/,
+  /android[\\/]app[\\/]build[\\/].*/,
+  /android[\\/]build[\\/].*/,
+  /android[\\/]build-cache[\\/].*/,
+  /android[\\/]\.cxx[\\/].*/,
+];
 
 // NativeWind reads global.css for the Tailwind layers; the Reanimated wrapper
 // adds the worklets-aware serializer. Order: NativeWind inside, Reanimated outside.
